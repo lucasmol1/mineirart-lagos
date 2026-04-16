5e99a5ce5b005b0b8d20ea51e75a17c9b0add057
lucasmol1 <lucasmol12@gmail.com>
2026-04-16 13:18:49 -0300

    Delete mineirart-lagos-main/mineirart4 directory

--- mineirart-lagos-main/mineirart4/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', () => {});
